8497f1a8850dea074be5b9b100d9267a0aee0eba
JoyoS58 <joyosugito99@gmail.com>
2024-04-30 09:06:54 +0700

    jobsheet 9

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,17 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 
-class UserModel extends Model
+class UserModel extends Model implements Authenticatable
 {
     use HasFactory;
+    use AuthenticatableTrait;
 
     protected $table = 'm_users';
     protected $primaryKey = 'user_id';
 
+    // @var array
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
     // protected $fillable = ['level_id', 'username', 'nama'];
 
