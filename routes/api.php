dac93c4161098655581095df354098a7556fbd3b
JoyoS58 <joyosugito99@gmail.com>
2024-05-05 23:32:22 +0700

    Jobshhet 10: 2. Membuat RESTful API Login

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +16,7 @@ use App\Http\Controllers\Api\RegisterController;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request){
+    return $request->user();
+});
