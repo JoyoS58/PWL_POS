aac3b2a7c472ba62c9b377c609e8bc0df437d37c
JoyoS58 <joyosugito99@gmail.com>
2024-05-05 23:46:50 +0700

    Jobsheet 10: 3.Membuat RESTful API Logout

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +23,4 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
